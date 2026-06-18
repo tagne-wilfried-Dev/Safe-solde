81aea605e057f7a8b095971697995978cc80b470
einsdev <tagnewilfried4@gmail.com>
2026-06-19 00:43:44 +0100

    le modelde donnees des transactions

---
+++ lib/models/transaction.dart
@@ -0,0 +1,37 @@
+class Transaction {
+  final int id;
+  final String title;
+  final double amount;
+  final bool isIncome;
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.isIncome,
+    required this.date,
+  });
+
+  // methode pour convertir l'objet en Map pour la sauvegarde JSON
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'isIncome': isIncome,
+      'date': date.toIso8601String(),
+    };
+  }
+
+  // Methode pour recreer l'objet a partir d'un Map
+  factory Transaction.fromMap(Map<String, dynamic> map) {
+    return Transaction(
+        id: map['id'],
+        title: map['title'],
+        amount: map['amount'],
+        isIncome: map['isIncome'],
+        date: DateTime.parse(map['date']),
+    );
+  }
+}
